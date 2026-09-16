db70faa244e5e5eebab5df710a40af8e31450ff9
Arju <arju.srivastav7@gmail.com>
2026-09-16 09:12:02 +0530

    Added controller layer

---
+++ src/controllers/userController.js
@@ -0,0 +1,11 @@
+const userService = require("../services/userService");
+
+const getUsers = (req, res) => {
+  const { minAge } = req.query;
+  userService.getUsers(minAge, (err, users) => {
+    if (err) return res.status(500).json({ message: "Internal Server Error" });
+    res.status(200).json(users);
+  });
+};
+
+module.exports = { getUsers };
